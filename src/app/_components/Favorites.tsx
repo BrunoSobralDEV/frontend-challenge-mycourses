e1246805303578866a136c3990a2545ab93ea5b0
Bruno Sobral <brunosobralss@hotmail.com>
2025-01-09 21:45:48 -0300

    feat: message to inform you that my favorites is empty

--- src/app/_components/Favorites.tsx
+++ src/app/_components/Favorites.tsx
@@ -1,7 +1,11 @@
 import * as S from './styled/Favorites.styled'
 import CourseCard from './CourseCard'
+import { Course } from './Courses'
+import svgHearthSearch from '/public/icons/heart-search.svg';
+import Image from 'next/image';
 
 export default function Favorites() {
+  // const courses: Course[] = []
   const courses = [
     {
       id: 1,
@@ -24,15 +28,23 @@ export default function Favorites() {
       thumbnail: '/images/banner.png',
     },
   ]
+
   return (
     <S.Section>
       <h1>Meus Favoritos</h1>
 
-      <div className="grid">
-        {courses.map((course) => (
+      <S.FavoritesWrapper $empty={courses.length === 0}>
+        {courses.length > 0 && courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
-      </div>
+        
+        {courses.length === 0 && (
+          <div>
+            <Image src={svgHearthSearch} alt="hearth-search icon"/>
+            <p>Parece que você ainda não tem cursos favoritados </p>
+          </div>
+        )}
+      </S.FavoritesWrapper>
     </S.Section>
   )
 }
--- src/app/_components/styled/Favorites.styled.tsx
+++ src/app/_components/styled/Favorites.styled.tsx
@@ -1,20 +1,31 @@
-'use client'
+"use client";
 import styled from "styled-components";
 
 export const Section = styled.section`
-  padding: 40px 20px;
+  padding: 40px 24px;
+  min-height: 301px;
 
   h1 {
-    margin-bottom: 16px;
-  }
-  
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 315px));
-    gap: 20px;
+    margin-bottom: 64px;
   }
 
-  @media screen and (min-width: ${({ theme }) => theme.screen.md }) {
+  @media screen and (min-width: ${({ theme }) => theme.screen.md}) {
     padding: 48px 60px;
   }
-`
+`;
+export const FavoritesWrapper = styled.div<{ $empty: boolean; }>`
+  display: ${(props) => (props.$empty ? "flex" : "grid")};
+  
+  grid-template-columns: ${(props) =>
+    props.$empty ? "none" : "repeat(auto-fit, minmax(250px, 315px))"};
+  gap: 20px;
+  text-align: center;
+  
+  align-items: ${(props) => (props.$empty ? "center" : "initial")};
+  justify-content: ${(props) => (props.$empty ? "center" : "initial")};
+  flex-direction: ${(props) => (props.$empty ? "column" : "initial")};
+  
+`;
+
+export const FavoritesEmpty = styled.section`
+`;
