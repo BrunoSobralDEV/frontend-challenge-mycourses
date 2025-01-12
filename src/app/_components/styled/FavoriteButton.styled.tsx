8f4b2147aebda4744e9518be28bb13f8708dc9fc
Bruno Sobral <brunosobralss@hotmail.com>
2025-01-12 19:05:26 -0300

    fix: space in the favorite button with text and its size for mobile screens

--- src/app/_components/styled/FavoriteButton.styled.tsx
+++ src/app/_components/styled/FavoriteButton.styled.tsx
@@ -5,26 +5,28 @@ export const FavoriteWrapper = styled.div<{ $btnLarge: boolean }>`
   ${({ theme, $btnLarge }) => css`
     position: relative;
     display: flex;
+    gap: 7px;
     justify-content: center;
     align-items: center;
-    width: ${$btnLarge ? "114px" : "36px"};
     height: ${$btnLarge ? "31px" : "36px"};
     border-radius: ${$btnLarge ? "0.125rem" : "50%"};
     background-color: ${$btnLarge ? "#FFF" : "rgba(0, 0, 0, 0.35)"};
     border: ${$btnLarge &&
     `1px solid ${theme.colors["border-favorite-button"]}`};
-
+    margin-bottom: 16px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
     cursor: pointer;
+    width: ${$btnLarge ? "" : "36px"};
 
     transition: all 0.3s ease;
 
     img {
       height: ${$btnLarge && "14px"};
+      transition: all 0.3s ease;
     }
 
     @media screen and (min-width: ${theme.screen.md}) {
-      
+      width: ${$btnLarge && "114px"};
     }
   `};
 `;
